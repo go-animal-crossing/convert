dc76b51baa238fd35e25b86b391f257c8b95d38d
Charles Marshall <cm56marshall@gmail.com>
2021-05-23 20:16:18 +0100

    start on the conversion

---
+++ convert/tansform_test.go
@@ -0,0 +1,9 @@
+package convert
+
+import (
+	"testing"
+)
+
+func Test_titles(t *testing.T) {
+
+}
---
+++ convert/util_test.go
@@ -0,0 +1,35 @@
+package convert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_SafeQuote(t *testing.T) {
+	original := `has "quotes" in 'it'`
+	expected := "has quotes in it"
+	actual := Safe(original)
+	assert.Equal(t, expected, actual, "Should be equal")
+}
+
+func Test_Title(t *testing.T) {
+	original := `is a title`
+	expected := "Is A Title"
+	actual := Title(original)
+	assert.Equal(t, expected, actual, "Should be equal")
+}
+
+func Test_SlugSingle(t *testing.T) {
+	original := `Is A Single`
+	expected := "is-a-single"
+	actual := Slugify(original)
+	assert.Equal(t, expected, actual, "Should be equal")
+}
+func Test_URL(t *testing.T) {
+	one := "Top level"
+	two := "2nd level"
+	expected := "/top-level/2nd-level"
+	actual := URL(one, two)
+	assert.Equal(t, expected, actual, "Should be equal")
+}
